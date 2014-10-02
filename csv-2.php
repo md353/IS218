d2fe87a56d5e2809138a58c42e1b1f78596df184
Mike <md353@njit.edu>
2014-10-02 17:06:00 -0400

    update

--- csv-2.php
+++ csv-2.php
@@ -1,97 +1,108 @@
 <?php
+
 //====================================================================
 //	Michael De La Cruz 
 //	IS218
-//	Assignment 2
+//	Challenge 1
 //====================================================================
 
-
-//**********************
-//	Created a class
-//**********************
-
-class csvfile 
-{
-	// defining variables
-	public $csv;
-	public $column_headings;
-//***************************************
-//	Created a function within the class
-//***************************************
-	public function readcsv($csv, $column_headings)
-	{
-//**************************
-//	Code to open CSV file 
-//**************************		
-	  ini_set('auto_detect_line_endings',TRUE);
-	  //	variable $csv is holding spot for csvfile to be opened
-		if (($handle = fopen($csv, "r")) !== FALSE) 
-		{
-    		while (($row = fgetcsv($handle, ",")) !== FALSE) 
-    		{
-     			if($column_headings == TRUE) 
-     			{
-       				$column_heading = $row;
-       				$column_headings = FALSE;
-     			} else {
-       				$record = array_combine($column_heading, $row);
-       				$records[] = $record;
-     				   }
-
-    		}
-
-    		fclose($handle);
-	  }
-/*
-	foreach($records as $record) {
-   		foreach($record as $key => $value) {
-   			//$i++;
-			//$record_num = $i - 1;
-		//$record = $records[$_GET[$record]];
-		//$sec_col = array_column($key, '1');
-		echo $sec_col;
-	//echo $key . ': ' . $value .  "</br> \n";
-    }
+//********************************************
+//	Created a class for reading the CSV File
+//********************************************	
+		class File {
+	
+		public function __construct() {
+			
+		}
+		
+		public static function openFile($file){
+			$handle = fopen($file, "r");
+			//$handle2 = fopen($file2, "r");
+			// $handle = array_replace_recursive($handle1,$handle2);
+			//$handle = 
+			//$handle = array_replace($handle1[],$handle2);
+			return $handle ;
+		}
+		
+		public static function closeFile($handle){
+			fclose($handle);
+		}
+		
+	}
+//***************************************************
+//	Created a class for checking the column headings 
+//***************************************************
+	class readcsv {
+		
+		public static $handle;
+		public static $colmn_headings;
+		
+		public function column_headingsCK($handle, $colmn_headings){
+			
+			while(($row = fgetcsv($handle, ",")) !== FALSE){
+					if($colmn_headings){
+						$column_heading = $row;
+						$colmn_headings = FALSE;
+					}
+					else{
+						$record = array_combine($column_heading, $row);
+						$records[] = $record;
+					}
+				}
+				return $records;
+		}
+		
 	}
 
-    echo '<hr>';
-  }
+//**********************************************************************************
+//	Created a class to print a table from the information extracted off the CSVfile
+//**********************************************************************************
+	class html_table {
+		
+		public static function printTable($records,$url_var){
+			if(isset($_GET[$url_var])){
+				echo '<table border="1">';
+				foreach($records[$_GET[$url_var]] as $key => $value){
+					
+					echo '<tr><th>' . $key . '</th>';
+					echo '<td>' . $value . '</td></tr>';
+				}
+					echo '</table>';
+			}
+		}
+	}
+	///class change extends html_table{
+	///	pblic static function 
+	//}
+//******************************************************
+//	Created a class create HTML links based off the class 
+//******************************************************
+	class pLinks extends html_table {
+		
+		public function __construct($records){
+			
+			$i = -1;
+			if(empty($_GET)){
+				foreach($records as $record){
+					$i++;
+				    echo '<a href="?record=' .$i. '">' . $record['INSTNM'] . '</a>';
+					echo'</p>';
+				}
+			}
+			
+			html_table::printTable($records, 'record');
+		}
+		//public function __destruct($records){
+			//html_table::printTable($records, 'record');
+		//}
 	}
 	
-*/		
+$csv = 'hd2013.csv';
+$file = File::openFile($csv);
 
-	if(empty($_GET)) 
-	{
-	foreach($records as $record) 
-	{
-		// creating records
-			$i++;
-			$record_num = $i - 1;
-			
-		// Printing out links
-      echo '<a href=' . '"http://localhost/is218/csv-2.php?record=' . $record_num . '"' . '>University ' . $i . ' </a>'; 		
-      echo '</p>';    	
- 	}
-	}
-		$record = $records[$_GET['record']];
-		
-	  echo "<table border='1'>";
-    	
-    	
-    	foreach($record as $key => $value) 
-    	{
-	  		echo "<tr>";
-	  		echo "<th> $key </th> <td> $value </td>";
-	 		echo "</tr>";
-    	}
-	  echo "</table>"; 
+$handle = new readcsv();
+$records = $handle->column_headingsCK($file, TRUE);
 
-  	}
-	}
-//*************
-//	Entering new CSV file into function to read 
-//*************
-	$newfile = new csvfile();
-	$newfile->readcsv("hd2013xl.csv",TRUE);
+new pLinks($records);
 
 ?>
